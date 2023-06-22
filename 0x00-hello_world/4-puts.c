e28de74439c9bb68e21e8ca11ddfab64f3305bd1
Daseg5 <meetsegund@gmail.com>
2023-06-22 15:55:31 -0700

    new commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * Entry: Always o (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle);
-    return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
