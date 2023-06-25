baf55b094151ef850fb72a677322e7466d8c3ff5
Daseg5 <meetsegund@gmail.com>
2023-06-25 08:49:47 -0700

    updated commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,10 @@
  *
  * Return: Always 0 (Success)
  */
+#include <stdio.h>
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art.\n");
-    	return (0)
+	printf("I am a piece of art,\nBut with proper grammar,\nYou can admire me,\nAnd my code as well.\n");
+	return 0;
 }
+
