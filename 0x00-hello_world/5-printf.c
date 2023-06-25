ddafa30e76a7bac5361edad49b5b14c1b887b895
Daseg5 <meetsegund@gmail.com>
2023-06-25 08:57:49 -0700

    updated commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,6 @@
  */
 int main(void)
 {
-	printf("I am a piece of art,\nBut with proper grammar,\nYou can admire me,\nAnd my code as well.\n");
+	printf("with proper grammar, but the outcome is a piece of art,,");
 	return 0;
 }
-
